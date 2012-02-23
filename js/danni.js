e83f31c3e747eff493c861a04218a2eba629c53e
Steven Surgnier <ssurgnier@gmail.com>
2012-02-23 01:34:50 -0800

    save point

--- js/danni.js
+++ js/danni.js
@@ -6,10 +6,11 @@ $(function(){
     routes: {
       '':         'toHome',
       'home':     'toHome',
-      'pictures': 'toPictures'
-      //'designs':  'toDesigns',
-      //'about':    'toAbout',
-      //'resume':   'toResume'
+      'pictures': 'toPictures',
+      'designs':  'toDesigns',
+      'about':    'toAbout',
+      'resume':   'toResume',
+      'resume/:section': 'resumeMenu'
     },
 
     toHome: function(){
@@ -24,28 +25,43 @@ $(function(){
       $('#page').hide();
       $('#page').html(view.render().el);
       $('#page').show();
-    }
+    },
 
-    //toDesigns: function(){
-      //var view = new DesignsView;
-      //$('#page').hide();
-      //$('#page').html(view.el);
-      //$('#page').show();
-    //},
+    toDesigns: function(){
+      var view = new DesignsView;
+      $('#page').hide();
+      $('#page').html(view.render().el);
+      $('#page').show();
+    },
 
-    //toAbout: function(){
-      //var view = new AboutView;
-      //$('#page').hide();
-      //$('#page').html(view.el);
-      //$('#page').show();
-    //},
+    toAbout: function(){
+      var view = new AboutView;
+      $('#page').hide();
+      $('#page').html(view.render().el);
+      $('#page').show();
+    },
     
-    //toResume: function(){
-      //var view = new ResumeView;
-      //$('#page').hide();
-      //$('#page').html(view.el);
-      //$('#page').show();
-    //}
+    toResume: function(){
+      var view = new ResumeView;
+      $('#page').hide();
+      $('#page').html(view.render().el);
+      $('#page').show();
+    },
+
+    resumeMenu: function(section){
+      switch(section){
+        case 'employment':
+          alert('employment');
+          break;
+        case default:
+        case 'education':
+          var view = new EducationView;
+          $('#resume-content').hide();
+          $('#resume-content').html(view.render().el);
+          $('#resume-content').show();
+          break;
+      }
+    },
   });
 
   // Views
@@ -57,9 +73,18 @@ $(function(){
 
     template: _.template($('#home-template').html()),
 
-    //initialize: function(){
-      //alert('Home');
-    //},
+    render: function(){
+      $(this.el).html(this.template());
+      return this;
+    }
+  });
+
+  window.AboutView = Backbone.View.extend({
+    tagName: 'div',
+
+    id: 'page-about',
+
+    template: _.template($('#about-template').html()),
 
     render: function(){
       $(this.el).html(this.template());
@@ -74,9 +99,31 @@ $(function(){
 
     template: _.template($('#pictures-template').html()),
 
-    //initialize: function(){
-      //alert('Pictures');
-    //},
+    render: function(){
+      $(this.el).html(this.template());
+      return this;
+    }
+  });
+
+  window.DesignsView = Backbone.View.extend({
+    tagName: 'div',
+
+    id: 'page-designs',
+
+    template: _.template($('#designs-template').html()),
+
+    render: function(){
+      $(this.el).html(this.template());
+      return this;
+    }
+  });
+
+  window.ResumeView = Backbone.View.extend({
+    tagName: 'div',
+
+    id: 'page-resume',
+
+    template: _.template($('#resume-template').html()),
 
     render: function(){
       $(this.el).html(this.template());
